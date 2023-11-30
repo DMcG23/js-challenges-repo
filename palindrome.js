10a5291877d325e23dc9c245ca5d415acff0d6a3
Danny McGuire <danny.mcguire97@gmail.com>
2023-11-30 15:24:13 +0100

    Altered comments
    Improved readability of comments

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,14 @@
 function palindrome(str) {
 // Regular expression that removes all non-alphanumeric characters.
     var regEx = /[\W_]/g;
-// Variable that creates a new string from the function's argument (str) by making it lowercase and replacing all non-alphanumeric character (regEx) with "".
+// Variable that creates a new string from the function's argument (str) 
+// by making it lowercase and replacing all non-alphanumeric character (regEx) with "".
    var smallStr = str.toLowerCase().replace(regEx, "");
-// Variable that reverses the new string created above (smallStr) by splitting, reversing and joining again.
+// Variable that reverses the new string created above (smallStr) by splitting, reversing 
+// and joining again.
    var reversedStr = smallStr.split("").reverse().join("");
-//An if statement that returns true if the original argument (str) matches the same argument when reversed.
+//An if statement that returns true if the original argument (str) matches the same argument 
+// when reversed.
    if (smallStr === reversedStr) {
     return true;
    }
